8589d9265abc08fd088f891c11792d19bc7eb4a8
meshcheriakov.b@nicecode.biz <meshcheriakov.b@nicecode.biz>
2022-01-19 18:12:10 +0300

    add(main): news request, parsed response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::error::Error;
+
+use serde::Deserialize;
+
+
+#[derive(Deserialize, Debug)]
+struct Articles {
+	articles: Vec<Article>
+}
+
+#[derive(Deserialize, Debug)]
+struct  Article {
+	title: String,
+	url: String
+}
+
+fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
+	let response = ureq::get(url).call()?.into_string()?;
+
+	let articles: Articles = serde_json::from_str(&response)?;
+	Ok(articles)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let url: &str = "https://newsapi.org/v2/top-headlines?country=us&apiKey=f426af2d83c14f71be38e24c3427a31e";
+	let articles = get_articles(url);
 }
